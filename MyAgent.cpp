4bf6b764c33bfe3784336d53c143dc6c9c936c30
Sebastian <56331266+SeabassJames@users.noreply.github.com>
2020-12-14 05:14:01 -0500

    Add files via upload

---
+++ MyAgent.cpp
@@ -0,0 +1,208 @@
+#include "MyAgent.h"
+#include "MyGame.h"
+#include <SDL_image.h>
+#include <SDL_ttf.h>
+#include <sstream>
+#include "mathtool/Box.h"
+
+namespace GMUCS425
+{
+
+  void MyAgent::handle_event(SDL_Event & e)
+  {
+    if(this->movable)
+    {
+      //do nothing now...
+    }
+  }
+
+  //update this agent's motion, looks, sound, etc
+  void MyAgent::update()
+  {
+    //do nothing by default...
+  }
+
+  void MyAgent::display()
+  {
+    if(!this->visible) return; //not visible...
+    //setup positions and ask sprite to draw something
+    this->sprite->display(x, y, scale, degree);
+  }
+
+  //show HUD (heads-up display) or status bar
+  void MyAgent::draw_HUD()
+  {
+    //draw nothing by defaut, your task to display the location of an agent
+    //on the upper left corner
+    //read http://lazyfoo.net/tutorials/SDL/16_true_type_fonts/index.php
+  }
+
+  bool MyAgent::collide(MyAgent * other)
+  {
+    mathtool::Box2d box1, box2;
+    box1.x=x;
+    box1.y=y;
+    box1.width=this->sprite->getWidth(scale);
+    box1.height=this->sprite->getHeight(scale);
+
+    box2.x=other->x;
+    box2.y=other->y;
+    box2.width=other->sprite->getWidth(other->scale);
+    box2.height=other->sprite->getHeight(other->scale);
+
+    return box1.intersect(box2);
+  }
+
+  void MyAgent::draw_bounding_box()
+  {
+    float my_W=this->sprite->getWidth(scale);
+    float my_H=this->sprite->getHeight(scale);
+
+    SDL_Rect box; //create a rect
+    box.x = x;  //controls the rect's x coordinate
+    box.y = y; // controls the rect's y coordinte
+    box.w = my_W; // controls the width of the rect
+    box.h = my_H; // controls the height of the rect
+
+    SDL_Renderer * renderer=getMyGame()->getRenderer();
+    SDL_SetRenderDrawColor(renderer,255,100,0,100);
+    //SDL_RenderDrawPoint(renderer, x, y);
+    SDL_RenderDrawRect(renderer,&box);
+
+    box.w/=10;
+    box.h=box.w;
+    SDL_SetRenderDrawColor(renderer,0,0,0,100);
+    SDL_RenderFillRect(renderer,&box);
+  }
+
+  void MyZombieAgent::display()
+  {
+    if(!this->visible) return; //not visible...
+    //setup positions and ask sprite to draw something
+    this->sprite->display(x, y, scale, degree, NULL, this->left?SDL_FLIP_NONE:SDL_FLIP_HORIZONTAL);
+    draw_bounding_box();
+  }
+
+  void MyZombieAgent::update()
+  {
+    if(!this->collision)
+    {
+      if(collision_free_timer>=0) this->collision_free_timer--;
+      else collide_with=NULL; //no collision
+    }
+
+    this->collision=false;
+
+    if(orig_x==INT_MAX)
+      orig_x=x;
+    if(left) x-=2;
+    else x+=2;
+    if(x<orig_x-100) left=false;
+    if(x>orig_x+100) left=true;
+  }
+
+  void MyZombieAgent::handle_event(SDL_Event & e)
+  {
+    //return;
+    if(this->collision && collide_with!=NULL)
+    {
+      //std::cout<<"already in collision with "<<collide_with<<std::endl;
+
+      return;
+    }
+
+    if(e.type==SDL_USEREVENT)
+    {
+      if(e.user.code == 1)
+      {
+        if(e.user.data1==this || e.user.data2==this)
+        {
+          MyAgent * other = (MyAgent *)((e.user.data1!=this)?e.user.data1:e.user.data2);
+
+          if(other!=collide_with)
+          {
+            collide_with=other;
+            left=!left;
+          }
+          this->collision_free_timer=10;
+          this->collision=true;
+        }
+        //else collide_with=NULL; //no collision
+      }
+    }
+  }
+
+  void MyChickenAgent::display()
+  {
+    //MyAgent::display();
+    if(!this->visible) return; //not visible...
+    //setup positions and ask sprite to draw something
+    this->sprite->display(x, y, scale, degree, NULL, this->ccw?SDL_FLIP_HORIZONTAL:SDL_FLIP_NONE);
+    draw_bounding_box();
+  }
+
+
+  void MyChickenAgent::handle_event(SDL_Event & e)
+  {
+    //std::cout<<"--------------------------------------"<<std::endl;
+    if(this->collision && collide_with!=NULL)
+    {
+      //std::cout<<"already in collision with "<<collide_with<<std::endl;
+
+      return;
+    }
+    //else
+    //  std::cout<<"NO in collision: this->collision="<<this->collision<<" with "<<collide_with<<std::endl;
+
+    if(e.type==SDL_USEREVENT)
+    {
+      if(e.user.code == 1)
+      {
+        if(e.user.data1==this || e.user.data2==this)
+        {
+          MyAgent * other = (MyAgent *)((e.user.data1!=this)?e.user.data1:e.user.data2);
+          if(other!=this->collide_with)
+          {
+            //std::cout<<"changed to: "<<other<<" from " << collide_with<<std::endl;
+            collide_with=other;
+            ccw=!ccw;
+          }
+          this->collision_free_timer=10;
+          this->collision=true;
+        }//end if
+      }//end if
+    }//end if
+  }
+
+  void MyChickenAgent::update()
+  {
+    //std::cout<<"this->collision_free_timer="<<this->collision_free_timer<<std::endl;
+
+    if(!this->collision)
+    {
+      if(collision_free_timer>=0) this->collision_free_timer--;
+      else collide_with=NULL; //no collision
+    }
+    this->collision=false;
+
+    if(radius==FLT_MAX)
+    {
+      radius=30;
+      center_x=x; center_y=y-radius;
+    }
+
+    float angle=(ccw)?0.1:-0.1;
+    float cos_t=cos(angle);
+    float sin_t=sin(angle);
+
+    float dx=x-center_x;
+    float dy=y-center_y;
+    float d=sqrt(dx*dx+dy*dy);
+    dx=dx/d;
+    dy=dy/d;
+
+    x=(int)((cos_t*dx-sin_t*dy)*radius)+center_x;
+    y=(int)((sin_t*dx+cos_t*dy)*radius)+center_y;
+  }
+
+}//end namespace
---
+++ MyAgent.h
@@ -0,0 +1,159 @@
+#pragma once
+
+#include "MySprite.h"
+#include "mathtool/Point.h"
+
+//#include "MyAgent.h"
+//#include "MyGame.h"
+//#include <SDL_image.h>
+//#include <SDL_ttf.h>
+//#include <sstream>
+#include "mathtool/Box.h"
+
+namespace GMUCS425
+{
+    //this defines the transform of the agent
+    class MyAgent
+    {
+      public:
+		  /*
+        //movable: true if this agent can move
+        //collision: true if this agent can collision with other agents
+        MyAgent(bool movable=true, bool collision=true)
+        {
+          x=y=degree=0;
+          scale=1;
+          visible=true;
+          sprite=NULL;
+          this->movable=movable;
+          this->collision=collision;
+        }
+		*/
+		//movable: true if this agent can move
+		//bird/squirrel collision: true if this agent can collision with other agents
+		MyAgent(bool movable = true, bool bcollision = true, bool scollision = true, bool collision = true)
+		{
+			x = y = degree = 0;
+			scale = 1;
+			visible = true;
+			sprite = NULL;
+			this->movable = movable;
+			this->birdcollision = bcollision;
+			this->squirrelcollision = scollision;
+			this->collision = collision;
+		}
+
+        //react to the events
+        virtual void handle_event(SDL_Event & e);
+
+        //update this agent's motion, looks, sound, etc
+        virtual void update();
+
+        //render this agent
+        virtual void display();
+
+        //show HUD (heads-up display) or status bar
+        virtual void draw_HUD();
+
+        //transforms
+        void rotate(float degree){ this->degree+=degree; }
+        void rotateTo(float degree){ this->degree=degree; }
+
+        void tranlate(float x, float y){ this->x+=x; this->y+=y; }
+        void tranlateTo(float x, float y){ this->x=x; this->y=y; }
+
+        void scaleTo(float s){ this->scale=s; }
+
+        //display
+        void show(){ visible=true; }
+        void hide(){ visible=false; }
+
+        //sprite, aks costume
+        void setSprite(MySprite * sprite){ this->sprite=sprite; }
+		MySprite* getSprite() { return this->sprite; }
+		MySprite* getSprite() const { return this->sprite; }
+		//float getWidth() const { return getSprite()->getWidth(scale); }
+		//float getHeight() const { return getSprite()->getHeight(scale); }
+
+        //motion/animation
+        //void glide(float x, float y, float seconds);
+
+        bool collide(MyAgent * other);
+		bool MyAgent::collide(MyAgent * other) const
+		{
+			mathtool::Box2d box1, box2;
+			box1.x = x;
+			box1.y = y;
+			box1.width = this->sprite->getWidth(scale);
+			box1.height = this->sprite->getHeight(scale);
+
+			box2.x = other->x;
+			box2.y = other->y;
+			box2.width = other->sprite->getWidth(other->scale);
+			box2.height = other->sprite->getHeight(other->scale);
+
+			return box1.intersect(box2);
+		}
+
+        //ACCESS METHODS
+        bool is_movable() const { return movable; }
+        bool is_visible() const { return visible; }
+
+        float getX() const { return x; }
+        float getY() const { return y; }
+        float getAngle() const { return degree; }
+        float getScale() const { return scale; }
+
+      protected:
+
+        void draw_bounding_box();
+
+        //current position and orientations
+        float x,y;
+        float degree; //rotation
+        float scale;
+
+    		bool visible;
+    		bool movable;
+    		bool collision;
+			bool birdcollision;
+			bool squirrelcollision;
+
+        MySprite * sprite; //current sprite
+        //it is possible that you can have more than one sprites
+        //vector<MySprite *> sprites; //the sprites
+    };
+
+    class MyZombieAgent : public MyAgent
+    {
+      public:
+        MyZombieAgent(bool movable=true, bool collision=true)
+        :MyAgent(movable, false, true){orig_x=INT_MAX; left=true; collide_with=NULL;}
+        virtual void update();
+        virtual void display();
+        virtual void handle_event(SDL_Event & e);
+      private:
+        int orig_x;
+        bool left;
+        MyAgent * collide_with;
+        int collision_free_timer=10;
+    };
+
+
+    class MyChickenAgent : public MyAgent
+    {
+      public:
+        MyChickenAgent(bool movable=true, bool collision=true):MyAgent(movable,false, true)
+        {radius=FLT_MAX; center_x=center_y=INT_MAX; ccw=false; collide_with=NULL;}
+        virtual void update();
+        virtual void display();
+        virtual void handle_event(SDL_Event & e);
+      private:
+        float radius;
+        int center_x, center_y;
+        bool ccw;
+        MyAgent * collide_with;
+        int collision_free_timer=10;
+    };
+
+}//end namespace
